13f67c44fce50becf1231696e93a37a5d2181523
Wioleta Krukowska <w.krukowska@polsource.com>
2019-10-30 14:04:21 +0100

    main class fix

--- src/main/java/com/regift/regift/ReGiftApplication.java
+++ src/main/java/com/regift/regift/ReGiftApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
 
+@Controller
 @SpringBootApplication
 public class ReGiftApplication {
 
