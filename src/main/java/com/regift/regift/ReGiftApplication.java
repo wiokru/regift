4da50079af958cebd0fac1c4a5f997eb59afb16f
Wioleta Krukowska <w.krukowska@polsource.com>
2019-10-30 13:57:57 +0100

    app basic setup pt.1

--- src/main/java/com/regift/regift/ReGiftApplication.java
+++ src/main/java/com/regift/regift/ReGiftApplication.java
@@ -1,13 +1,29 @@
 package com.regift.regift;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ReGiftApplication {
 
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+//	@Autowired
+//	private DataSource dataSource;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReGiftApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String index() {
+		return "index";
 	}
 
 }
