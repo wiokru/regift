7585ed3a35c9354c9ab6280468d2543f8d7164a0
Wioleta Krukowska <w.krukowska@polsource.com>
2019-11-04 14:20:27 +0100

    created home template

--- src/main/java/com/regift/regift/controllers/HomeController.java
+++ src/main/java/com/regift/regift/controllers/HomeController.java
@@ -2,12 +2,13 @@ package com.regift.regift.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
 
     @RequestMapping("/")
-    public String home(){
-        return "home";
+    public ModelAndView home(){
+        return new ModelAndView("home");
     }
 }
