bb341fc681e3ce25b1a486e1e18b3515634a2ef8
Wioleta Krukowska <w.krukowska@polsource.com>
2019-11-26 13:19:21 +0100

    added signup page

---
+++ src/main/java/com/regift/regift/controllers/SignUpController.java
@@ -0,0 +1,14 @@
+package com.regift.regift.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class SignUpController {
+
+    @RequestMapping("/signup")
+    public ModelAndView signup(){
+        return new ModelAndView("signup");
+    }
+}
---
+++ src/main/java/com/regift/regift/utils/User.java
@@ -0,0 +1,4 @@
+package com.regift.regift.utils;
+
+public class User {
+}
