ea4afe7cda6c6095eab7621e0e36b8e394371042
Wioleta Krukowska <w.krukowska@polsource.com>
2019-11-26 13:41:54 +0100

    created datasource bean

--- src/main/java/com/regift/regift/ReGiftApplication.java
+++ src/main/java/com/regift/regift/ReGiftApplication.java
@@ -1,17 +1,10 @@
 package com.regift.regift;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReGiftApplication {
-
-	@Value("${spring.datasource.url}")
-	private String dbUrl;
-
-//	@Autowired
-//	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReGiftApplication.class, args);
--- src/main/java/com/regift/regift/controllers/HomeController.java
+++ src/main/java/com/regift/regift/controllers/HomeController.java
@@ -1,14 +1,39 @@
 package com.regift.regift.controllers;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @RestController
 public class HomeController {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Autowired
+    private DataSource dataSource;
 
     @RequestMapping("/")
     public ModelAndView home(){
         return new ModelAndView("home");
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
     }
 }
