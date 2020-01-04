5bdddd74c3ca657e2cd942333226a5b9585b4e10
Wioleta Krukowska <wioleta.krukowska@gmail.com>
2020-01-04 19:49:26 +0100

    database

--- src/main/java/com/regift/regift/ReGiftApplication.java
+++ src/main/java/com/regift/regift/ReGiftApplication.java
@@ -5,15 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ReGiftApplication {
-
-//	@Value("${spring.datasource.url}")
-//	private String dbUrl;
-
-//	@Autowired
-//	private DataSource dataSource;
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReGiftApplication.class, args);
 	}
--- src/main/java/com/regift/regift/controllers/SignUpController.java
+++ src/main/java/com/regift/regift/controllers/SignUpController.java
@@ -1,24 +1,27 @@
 package com.regift.regift.controllers;
 
 import com.regift.regift.utils.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.sql.DataSource;
 
 @RestController
 public class SignUpController {
 
-    @RequestMapping("/signup")
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/signup")
     public ModelAndView signup(Model model) {
         model.addAttribute("tempUser", new User());
         return new ModelAndView("signup");
     }
 
     @PostMapping("/signup")
-    public void signUp(@ModelAttribute("tempUser") User user){
-        System.out.println(user.getEmail());
+    public void signUp(@ModelAttribute("name") String name){
+        System.out.println(name);
     }
 }
