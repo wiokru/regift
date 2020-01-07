bf940e122b4b8cad8332fb379420cfe167f56a7c
Wioleta Krukowska <wioleta.krukowska@gmail.com>
2020-01-07 12:26:02 +0100

    database

--- src/main/java/com/regift/regift/ReGiftApplication.java
+++ src/main/java/com/regift/regift/ReGiftApplication.java
@@ -2,12 +2,10 @@ package com.regift.regift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ReGiftApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ReGiftApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReGiftApplication.class, args);
+    }
 }
