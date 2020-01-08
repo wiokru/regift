04078ccd4b8505377994a56fc499a95e4472929f
Wioleta Krukowska <w.krukowska@polsource.com>
2020-01-08 13:57:48 +0100

    cleanups

--- src/main/java/com/regift/regift/controllers/SignUpController.java
+++ src/main/java/com/regift/regift/controllers/SignUpController.java
@@ -15,25 +15,29 @@ public class SignUpController {
     @Autowired
     private UserRepository userRepository;
 
+//    @GetMapping("/signup")
+//    public ModelAndView signup(Model model) {
+//        return new ModelAndView("signup");
+//    }
+
     @GetMapping("/signup")
-    public ModelAndView signup(Model model) {
-        return new ModelAndView("signup");
+    public String signup(Model model) {
+        return "signup";
     }
 
     //@PostMapping("/signup")
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
     public String registerUser(Model model,
-                        @ModelAttribute("name") String name,
-                        @ModelAttribute("surname") String surname,
-                        @ModelAttribute("email") String email,
-                        @ModelAttribute("city") String city,
-                        @ModelAttribute("passowrd") String password) {
+                               @ModelAttribute("name") String name,
+                               @ModelAttribute("surname") String surname,
+                               @ModelAttribute("email") String email,
+                               @ModelAttribute("city") String city,
+                               @ModelAttribute("passowrd") String password) {
         try {
             User user = new User(email, name, surname, password, city);
             userRepository.save(user);
             return "login";
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             model.addAttribute("message", e.getMessage());
             System.out.println("XXXXXXXXXXXXXXXXXXXXXX" + e.getMessage() + '\n' + e.getLocalizedMessage());
             return "error";
