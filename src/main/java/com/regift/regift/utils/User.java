248556ae69d19733c6e97e6f4a8f466533c9e106
Wioleta Krukowska <w.krukowska@polsource.com>
2020-01-08 12:23:54 +0100

    database

--- src/main/java/com/regift/regift/utils/User.java
+++ src/main/java/com/regift/regift/utils/User.java
@@ -18,7 +18,7 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 //    @Column(name = "id")
-    private int id;
+    private Long id;
     //    @Column(name = "email", nullable = false, unique = true)
     private String email;
 //    @Column(name = "name")
@@ -40,11 +40,11 @@ public class User implements Serializable {
         this.city = city;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
