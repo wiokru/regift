ee2ac79a1740be2d4560fbf004ef9d43b8df5ef8
Wioleta Krukowska <wioleta.krukowska@gmail.com>
2020-01-25 17:37:08 +0100

    add editing user details

--- src/main/java/com/regift/regift/controllers/UserHomeController.java
+++ src/main/java/com/regift/regift/controllers/UserHomeController.java
@@ -4,9 +4,7 @@ import com.regift.regift.utils.PostRepository;
 import com.regift.regift.utils.User;
 import com.regift.regift.utils.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
@@ -20,32 +18,58 @@ public class UserHomeController {
     @Autowired
     private PostRepository postRepository;
 
+    private User currentUser;
+
     @GetMapping("/user/{id}/home")
     public ModelAndView userHomePage(@PathVariable("id") Long id) {
         ModelAndView modelAndView = new ModelAndView("user_home");
         modelAndView.addObject("postList", postRepository.findAll());
 
-        Optional<User> currentUser = userRepository.findById(id);
-        modelAndView.addObject("currentUser", currentUser.get());
+        this.currentUser = userRepository.findById(id).get();
+        modelAndView.addObject("currentUser", this.currentUser);
         return modelAndView;
     }
 
     @GetMapping("/user/{id}/my_posts")
     public ModelAndView userPostPage(@PathVariable("id") Long id) {
         ModelAndView modelAndView = new ModelAndView("user_posts");
-        Optional<User> currentUser = userRepository.findById(id);
+//        Optional<User> currentUser = userRepository.findById(id);
 
-        modelAndView.addObject("postList", postRepository.findByUser(currentUser.get()));
-        modelAndView.addObject("currentUser", currentUser.get());
+        modelAndView.addObject("postList", postRepository.findByUser(currentUser));
+        modelAndView.addObject("currentUser", currentUser);
         return modelAndView;
     }
 
     @GetMapping("/user/{id}/my_info")
     public ModelAndView userInfoPage(@PathVariable("id") Long id) {
         ModelAndView modelAndView = new ModelAndView("user_info");
-        Optional<User> currentUser = userRepository.findById(id);
+//        Optional<User> currentUser = userRepository.findById(id);
 
-        modelAndView.addObject("currentUser", currentUser.get());
+        modelAndView.addObject("currentUser", currentUser);
         return modelAndView;
+    }
+
+    @PostMapping("/user/{id}/my_info")
+    public ModelAndView saveEditUserInfo(@PathVariable("id") Long id,
+                                         @ModelAttribute("name") String name,
+                                         @ModelAttribute("surname") String surname,
+                                         @ModelAttribute("email") String email,
+                                         @ModelAttribute("city") String city) {
+        try {
+            currentUser.setName(name);
+            currentUser.setSurname(surname);
+            currentUser.setEmail(email);
+            currentUser.setCity(city);
+
+            userRepository.save(currentUser);
+            ModelAndView modelAndView = new ModelAndView("redirect:/user/" + id.toString() + "/my_info");
+            modelAndView.addObject("info_message", "User updated successfully.");
+            return modelAndView;
+        }
+        catch (Exception e) {
+            ModelAndView modelAndView = new ModelAndView("redirect:/user/" + id.toString() + "/my_info");
+            modelAndView.addObject("error_message", e.getMessage());
+            return modelAndView;
+        }
     }
 }
