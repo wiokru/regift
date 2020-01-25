bace0a474fadae0c31d3fd1cb86f6a7f79018ec1
Wioleta Krukowska <wioleta.krukowska@gmail.com>
2020-01-25 11:51:56 +0100

    added elements on user interface

--- src/main/java/com/regift/regift/controllers/UserHomeController.java
+++ src/main/java/com/regift/regift/controllers/UserHomeController.java
@@ -1,12 +1,15 @@
 package com.regift.regift.controllers;
 
 import com.regift.regift.utils.PostRepository;
+import com.regift.regift.utils.User;
 import com.regift.regift.utils.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @RestController
 public class UserHomeController {
@@ -18,7 +21,31 @@ public class UserHomeController {
     private PostRepository postRepository;
 
     @GetMapping("/user/{id}/home")
-    public ModelAndView userHomePage(@PathVariable String id) {
-        return new ModelAndView("user_home");
+    public ModelAndView userHomePage(@PathVariable("id") Long id) {
+        ModelAndView modelAndView = new ModelAndView("user_home");
+        modelAndView.addObject("postList", postRepository.findAll());
+
+        Optional<User> currentUser = userRepository.findById(id);
+        modelAndView.addObject("currentUser", currentUser.get());
+        return modelAndView;
+    }
+
+    @GetMapping("/user/{id}/my_posts")
+    public ModelAndView userPostPage(@PathVariable("id") Long id) {
+        ModelAndView modelAndView = new ModelAndView("user_posts");
+        Optional<User> currentUser = userRepository.findById(id);
+
+        modelAndView.addObject("postList", postRepository.findByUser(currentUser.get()));
+        modelAndView.addObject("currentUser", currentUser.get());
+        return modelAndView;
+    }
+
+    @GetMapping("/user/{id}/my_info")
+    public ModelAndView userInfoPage(@PathVariable("id") Long id) {
+        ModelAndView modelAndView = new ModelAndView("user_info");
+        Optional<User> currentUser = userRepository.findById(id);
+
+        modelAndView.addObject("currentUser", currentUser.get());
+        return modelAndView;
     }
 }
